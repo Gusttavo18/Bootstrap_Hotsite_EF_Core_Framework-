e1081a294c686779127e997d6638a9218455bb43
Gustavo <gustavohenriquedias14@gmail.com>
2021-12-29 17:19:53 -0300

    Enviado

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+                DatabaseService dbs = new DatabaseService();
+                dbs.CadastraInteresse(cad);
+                return Json(new{status="OK"}); //mensagem que sinaliza no Logs se o formulário foi enviado com sucesso
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("REGISTRO DE LOGGS DO CADASTRO!!!" + e.Message);
+                return Json(new{status="ERR", mensagem = "Erro ao cadastrar!"});  //mensagem que sinaliza no Logs se o formulário deu erro 
+            }
+
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿function Cadastrar()
+{
+    let parametros = {
+        Nome: $("#nome").val(),
+        Email: $("#email").val(),
+        Mensagem: $("#mensagem").val(),
+    }
+    $.post("/Home/Cadastrar", parametros)
+    
+    .done(function(data)
+    {
+        if (data.status=="OK") 
+        {
+            $("#form").after("<h3> Seu Cadastro foi registrado com sucesso!</h3>");
+            $("#form").hide();
+        } else {
+            $("#form").after("<h3> Erro! Tente novamente mais tarde.</h3>");
+            $("#form").hide();
+        }    
+    })
+    .fail(function()
+    {
+        alert("Erro no servidor");
+    });
+}
+
+$(document).ready(function()
+{
+    $("#frmCadastro").submit(function(e)
+    {
+        e.preventDefault();
+        Cadastrar();
+    });
+});
